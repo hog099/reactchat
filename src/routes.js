929a47c8ac40f3f4ea755a0328954be5608c8e92
hog099 <oliveiraginelli@gmail.com>
2019-09-15 12:17:57 -0300

    finalizado piloto

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import AuthorApp from './pages/auth/authorpainel'
-import Start from './pages/index'
+import Home from './pages/index'
 import HomeClient from './pages/chatClient/index'
 import Painel from './pages/painel/index'
 
 function Routes() {
     return (
         <Switch>
-            <Route path="/" exact component={Start} />
+            <Route path="/" exact component={Home} />
             <Route path="/homeClient" exact component={HomeClient} />
             <Route path="/login" exact component={AuthorApp} />
             <Route path="/painel" exact component={Painel} />
