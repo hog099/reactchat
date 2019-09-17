0e126a90f11eed4ecb231708506a614f6062174a
hog099 <oliveiraginelli@gmail.com>
2019-09-16 23:08:47 -0300

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 // const firebase = require('firebase')
 
 var _config = {
-    apiKey: "AIzaSyBM9z6ZCIKKM1mkS8lxVH-dHQiyJIio8eM",
-    authDomain: "reactchat-7d437.firebaseapp.com",
-    databaseURL: "https://reactchat-7d437.firebaseio.com",
-    projectId: "reactchat-7d437",
+    apiKey: "######",
+    authDomain: "project-firebase.firebaseapp.com",
+    databaseURL: "https://project-firebase.firebaseio.com",
+    projectId: "project-firebase",
     storageBucket: "",
-    messagingSenderId: "776041580344",
-    appId: "1:776041580344:web:2755fbb0218e0f28a7e3cd"
+    messagingSenderId: "11111111111",
+    appId: "1111111111111111"
 };
 
 
